4f50d1d47c64b03421282836310e27be317f511b
Vladimir Martynov <92070126+q0b3rMAN@users.noreply.github.com>
2025-07-11 16:32:44 +1000

    Update AvitoParser.php

--- app/Services/AvitoParser.php
+++ app/Services/AvitoParser.php
@@ -33,7 +33,7 @@ class AvitoParser
             '--no-sandbox',
             '--disable-dev-shm-usage',
             '--window-size=1280,720',
-            //'--user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.7204.100 Safari/537.36"'
+            '--user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.7204.100 Safari/537.36"'
         ]);
 
         $options->setExperimentalOption('prefs', [
@@ -61,7 +61,7 @@ class AvitoParser
         $driver->get($url);
 
         $driver->manage()->timeouts()->implicitlyWait(5);
-        file_put_contents(storage_path('app/debug.html'), $driver->getPageSource());
+        //file_put_contents(storage_path('app/debug.html'), $driver->getPageSource());
 
         $countText = $driver->findElement(WebDriverBy::cssSelector('[data-marker="page-title/count"]'))->getText();
         $driver->quit();
